64e969f3b9d41ed616414a9a5aaeca90d5d7337b
Yu Hin Li <yuhinl@student.unimelb.edu.au>
2022-07-23 10:31:47 +1000

    init commit
    Create project

---
+++ iGallery/iGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iGallery
+//
+//  Created by Yu Hin Li on 23/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
